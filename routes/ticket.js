f8ce6d454513b6e35e9f84208e9efe37e6744395
Louis Pham <louispham23@gmail.com>
2020-03-20 12:04:27 -0700

    added ticket get

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,4 +1,11 @@
 const router = require("express").Router();
 const con = require("../connection");
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  con.query("SELECT * FROM heroku_4626acc8b075c22.ticket", (err, rows) => {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
+
+module.exports = router;
