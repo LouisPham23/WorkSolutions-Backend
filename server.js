9c52762402adccb1158ffc736448ed8bddd3f2d8
Louis Pham <louispham23@gmail.com>
2020-03-24 09:31:21 -0700

    ignore origin

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:3000/",
+    // origin: "http://localhost:3000/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
   })
 );
