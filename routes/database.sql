cd7c0862c3e03b6297766992f5b9c0bbfb9260ad
jcapper94 <jared.capper@gmail.com>
2020-04-08 20:28:59 -0700

    updated SQL

--- routes/database.sql
+++ routes/database.sql
@@ -2,49 +2,52 @@ CREATE TABLE TICKET(
   Ticket_number INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   Ticket_type VARCHAR(50),
   Priority INT,
-  Status VARCHAR(50),
+  Status_Id VARCHAR(50),
   Title VARCHAR(255),
   Created_By INT NOT NULL,
   Assigned_To INT,
   Assigned_Date DATE,
   Deadline_Date DATE,
   Description TEXT,
-  FOREIGN KEY (Created_By) REFERENCES EMPLOYEE(Employee_id) ON DELETE CASCADE
+  FOREIGN KEY (Created_By)    REFERENCES EMPLOYEE(Employee_Id),
+    FOREIGN KEY (Assigned_To) REFERENCES EMPLOYEE(Employee_Id),
+      FOREIGN KEY (Status_Id) REFERENCES STATUS(Status_Id)
 );
 
 CREATE TABLE DEPARTMENT(
-  Department_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  Department_Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   Name VARCHAR(50),
   Location VARCHAR(255),
   Acronym VARCHAR(50)
 );
 
 CREATE TABLE TEAM(
-  Team_id INT NOT NULL AUTO_INCREMENT,
-  Department_id INT NOT NULL,
-  Name VARCHAR(100),
-  CONSTRAINT PRIMARY KEY TEAM(Team_id, Department_id),
-  FOREIGN KEY (Department_id) REFERENCES DEPARTMENT(Department_id)
+  Team_Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  Team_name VARCHAR(100)
 );
 
 CREATE TABLE EMPLOYEE(
-  Employee_id VARCHAR(10) PRIMARY KEY NOT NULL,
+  Employee_Id VARCHAR(10) PRIMARY KEY NOT NULL,
   First_name VARCHAR(100),
   Last_name VARCHAR(100),
-  Group_id INT,
-  Department_id INT,
-  Classification VARCHAR(100),
-  FOREIGN KEY (Group_id) REFERENCES TEAM(Team_id),
-  FOREIGN KEY (Department_id) REFERENCES DEPARTMENT(Department_id)
+  Department_Id INT,
+  Type VARCHAR(100),
+  Specialty VARCHAR(100),
+  Levels VARCHAR(100),
+  FOREIGN KEY (Department_Id) REFERENCES DEPARTMENT(Department_Id)
 );
 
-<<<<<<< HEAD
-
 CREATE TABLE STATUS (
-    Status_ID int,
+    Status_Id int,
     Status_name VARCHAR(15)
 );
 
+CREATE TABLE TEAM_EMPLOYEE(
+  Team_Id INT NOT NULL,
+  Employee_Id INT NOT NULL,
+  FOREIGN KEY (Team_Id) REFERENCES TEAM(Team_Id),
+  FOREIGN KEY (Employee_Id) REFERENCES EMPLOYEE(Employee_Id)
+);
 
 /* trigger for insert
 //Delimiter
@@ -55,13 +58,11 @@ Create Trigger
 
 DELIMITER //
 
-=======
 DELIMITER // 
 CREATE PROCEDURE GetTeamsAndMembers()
 BEGIN
     SELECT *
     FROM 
-	    TEAM T JOIN EMPLOYEE E ON E.Group_id = T.Group_id;
+	    TEAM T JOIN EMPLOYEE E ON E.Team_Id = T.Team_Id;
 END //
 DELIMITER ;
->>>>>>> 263853e3d5106351efd5bfc7306b608237e9685f
