d916349de69d318ef320f3955baefd30446eb364
Louis Pham <louispham23@gmail.com>
2020-04-18 20:53:29 -0700

    added team_employee rows

--- routes/team.js
+++ routes/team.js
@@ -4,6 +4,13 @@ const router = express.Router();
 const con = require("../connection");
 
 router.get("/", (req, res) => {
+  con.query("SELECT * FROM TEAM", (err, rows) => {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
+
+router.get("/", (req, res) => {
   con.query("Call GetTeamsAndMembers", (err, rows) => {
     if (err) throw err;
     res.send(rows);
