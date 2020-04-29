57f2b235126d7f132bb389b2f4b5a9b13b0061b2
Louis Pham <louispham23@gmail.com>
2020-04-29 13:08:12 -0700

    put request for ticket

--- routes/team.js
+++ routes/team.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   con.query(
-    `SELECT Team_name, TE.Team_Id, E.First_name, E.Last_name
+    `SELECT Team_name, TE.Team_Id, E.First_name, E.Last_name, E.Employee_Id
       FROM TEAM_EMPLOYEE TE 
         JOIN EMPLOYEE E ON E.Employee_Id = TE.Employee_Id
         JOIN TEAM T ON TE.Team_Id = T.Team_Id
@@ -35,4 +35,8 @@ router.post("/", (req, res) => {
   });
 });
 
+router.delete("/:id", (req, res) => {
+  let team_id = req.params.id;
+  res.send(team_id);
+});
 module.exports = router;
